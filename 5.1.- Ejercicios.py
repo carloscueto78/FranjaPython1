98eaecd76e4d4ea05daad51be1414df836e5152a
carloscueto78 <carlos.cueto@alumnos.upla.cl>
2022-04-27 16:03:23 -0400

    a

---
+++ 5.1.- Ejercicios.py
@@ -0,0 +1,16 @@
+# Ejercicio 1
+# Escribir un programa que pregunte el nombre por consola
+# y un numero entero e imprimir por pantalla el nombre
+# tantas veces el numero que introdujo
+
+
+numero1 = int(input("Ingrese su numero:"))
+print(numero1)
+nombre1 = input("Ingrese su nombre:")
+print(numero1 * nombre1)
+
+
+
+
+
+
